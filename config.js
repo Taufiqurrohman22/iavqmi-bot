8015e8c62745db03615a32727022f6ed4474aa0d
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-10-29 06:56:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.owner = ['6283822205585']
+global.ownernomer = "6283822205585"
+global.premium = ['6283822205585']
+global.packname = 'Sticker By Iavqmi'
+global.author = 'Opik'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
